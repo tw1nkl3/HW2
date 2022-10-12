4aa1d0b9033461d1a38c4b9d3002502aca2cfffd
Ivan <grabovivan@gmail.com>
2022-10-12 19:53:51 +0300

    num2 added

---
+++ num2/Program.cs
@@ -0,0 +1,11 @@
+﻿int num = int.Parse(Console.ReadLine());
+
+if (num >= 100)
+{
+    string temp =  Convert.ToString(num);
+    Console.WriteLine("3-ья цифра: " + temp[2]);
+}
+else
+{
+    Console.WriteLine("Нет третьей цифры");
+}
