902084cb094cc2235844de319abdc2d00bade51e
Ivan <grabovivan@gmail.com>
2022-10-12 19:58:37 +0300

    num3 added

---
+++ num3/Program.cs
@@ -0,0 +1,17 @@
+﻿int num = int.Parse(Console.ReadLine());
+
+if (num >= 1 && num <= 7)
+{
+    if (num == 6 | num == 7)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+else
+{
+    Console.WriteLine("Не день недели");
+}
