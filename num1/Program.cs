bb89a330ab81fa8ee931dbfdb58e37037a6372d2
Ivan <grabovivan@gmail.com>
2022-10-12 19:27:34 +0300

    num1 added

---
+++ num1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+if ((num >= 100 && num <= 999) | (num >= -999 && num <= -100))
+{
+    int temp = num / 10;
+    //int secondDigit = temp % 10;
+    Console.WriteLine("Вторая цифра: " + Math.Abs(temp % 10));
+}
+else
+{
+    Console.WriteLine("Неверный ввод");
+}
